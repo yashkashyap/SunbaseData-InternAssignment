ee9acf6bf39a66aa4ae64c186749f185f6f567db
yashkashyap <yash00kash@gmail.com>
2023-10-20 16:22:58 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 model = pickle.load(open('model.pkl', 'rb'))
 
